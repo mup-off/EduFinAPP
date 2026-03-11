263bdbb4949e51f5c2cf566e0ed45e5fde1a8825
MUPENZI Fulgence <mupenz250@gmail.com>
2026-03-11 21:14:54 +0200

    Implemented EduFinApp Django-project and Basic Setup

--- EduFinApp/urls.py
+++ EduFinApp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import testing_view
+from core.views import testing_view, testing_detail_view
 from core.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing', testing_view, name='testing'),
+    path('testing/<int:id>', testing_detail_view, name='testing'),
     path('health', health_check, name = 'Health')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from core.models import Testing
 from core.serializers import TestingSerializer
 from rest_framework.decorators import api_view
@@ -17,6 +17,6 @@ def health_check(request):
     return JsonResponse ({'status': 'OK'})
 
 def testing_detail_view(request, id):
-    data = Testing.objects.all()
-    serializer = TestingSerializer(data, many=True)
-    return Response(serializer.data)
+    test_data = get_object_or_404(Testing, id=id)
+    serializer = TestingSerializer(test_data, many=True)
+    return Response(serializer.test_data, safe=False)
