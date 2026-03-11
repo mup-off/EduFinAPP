a6274229a9449d0eb714406445a5e2d0c9b3bf1d
MUPENZI Fulgence <mupenz250@gmail.com>
2026-03-11 20:54:45 +0200

    Implemented EduFinApp Django-project and Basic Setup

--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from core.models import Testing
+from core.serializers import TestingSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(["GET"])
 def testing_view(request):
-    # For now, return a simple static JSON response
-    return JsonResponse({'message': 'Hello, world!'})
+    
+    data = Testing.objects.all()
+    serializer = TestingSerializer(data, many=True)
+
+    return Response(serializer.data)
 def health_check(request):
     return JsonResponse ({'status': 'OK'})
+
+def testing_detail_view(request, id):
+    data = Testing.objects.all()
+    serializer = TestingSerializer(data, many=True)
+    return Response(serializer.data)
